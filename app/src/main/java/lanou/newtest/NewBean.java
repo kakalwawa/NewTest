d4de06a6dd37c974c0cab4af1eb19339cab7e3c4
翔 <706655044@qq.com>
2016-09-12 11:20:32 +0800

    dddd

---
+++ app/src/main/java/lanou/newtest/NewBean.java
@@ -0,0 +1,20 @@
+package lanou.newtest;
+
+/**
+ * Created by dllo on 16/9/12.
+ */
+public class NewBean  {
+   private int a ;
+
+    public NewBean() {
+
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
