95b7e5231571576878f230ce4c519bcf689ab496
dllo <dllo@dllodeMac-mini-334.local>
2016-09-12 11:12:50 +0800

    石宇佳第一天的任务!!

---
+++ app/src/main/java/lanou/newtest/Shi.java
@@ -0,0 +1,31 @@
+package lanou.newtest;
+
+/**
+ * 石宇佳第一天的任务
+ * Created by dllo on 16/9/12.
+ */
+public class Shi {
+    private static final String TAG = "TAG_Shi";
+    private int a;
+    private String b;
+
+    public static String getTAG() {
+        return TAG;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+}
