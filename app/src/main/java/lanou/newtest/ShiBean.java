5b57397a58fd2b473b294dc4df62bd7bbece2334
dllo <dllo@dllodeMac-mini-334.local>
2016-09-12 11:31:02 +0800

    这次对了么

---
+++ app/src/main/java/lanou/newtest/ShiBean.java
@@ -0,0 +1,15 @@
+package lanou.newtest;
+
+/**
+ * Created by dllo on 16/9/12.
+ */
+public class ShiBean {
+    private static final String TAG = "TAG_ShiBean";
+    private int shiAge;
+    private String shiName;
+
+    public ShiBean(int shiAge, String shiName) {
+        this.shiAge = shiAge;
+        this.shiName = shiName;
+    }
+}
